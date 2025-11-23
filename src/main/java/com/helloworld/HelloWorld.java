e95ceecc9794ab91fb20bffb7a76187ebfd91f5f
John Doe <georgios.avdeliotis.001@student.uni.lu>
2025-11-23 21:11:18 +0100

    Updated HelloWorld.java

--- src/main/java/com/helloworld/HelloWorld.java
+++ src/main/java/com/helloworld/HelloWorld.java
@@ -1,12 +1,13 @@
 package com.helloworld;
 
+import org.slf4j.*;
+
 public class HelloWorld {
+    private static final Logger log = LoggerFactory.getLogger(HelloWorld.class);
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("This is a Java application for security monitoring.");
-        
-        String javaVersion = System.getProperty("java.version");
-        String javaVendor = System.getProperty("java.vendor");
-        System.out.println("Running on Java " + javaVersion + " (" + javaVendor + ")");
+        log.info("Hello, World!");
+        log.info("This is a Java application for security monitoring.");
+        log.info("Running on Java {}", System.getProperty("java.version"));
+
     }
 }
