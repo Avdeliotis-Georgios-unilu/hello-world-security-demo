b433cd8c0f3cd0ffe2d7042f2b37d2e65d2df75b
John Doe <georgios.avdeliotis.001@student.uni.lu>
2025-11-28 10:33:45 +0100

    Removed log implementation

--- src/main/java/com/helloworld/HelloWorld.java
+++ src/main/java/com/helloworld/HelloWorld.java
@@ -1,13 +1,10 @@
 package com.helloworld;
 
-import org.slf4j.*;
-
 public class HelloWorld {
-    private static final Logger log = LoggerFactory.getLogger(HelloWorld.class);
     public static void main(String[] args) {
-        log.info("Hello, World!");
-        log.info("This is a Java application for security monitoring.");
-        log.info("Running on Java {}", System.getProperty("java.version"));
+        System.out.println("Hello, World!");
+        System.out.println("This is a Java application for security monitoring.");
+        System.out.println("Running on Java " + System.getProperty("java.version"));
 
     }
 }
